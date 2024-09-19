4e05e21b190c7834a673b25e1ccd27968c1ac63f
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2024-09-19 08:40:05 -0700

    First

---
+++ DP_BattleRoyale/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    public int maxPlayers = 10;
+
+    public static NetworkManager instance;
+
+    void Awake ()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster ()
+    {
+        //Debug.Log("We have connected to master server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void CreateRoom (string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    public void JoinRoom (string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangeScene (string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+}
---
+++ DP_BattleRoyale/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviourPun
+{
+
+    [Header("Stats")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Components")]
+    public Rigidbody rig;
+
+    public int id;
+    public Player photonPlayer;
+
+    void Update ()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            TryJump();
+
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 dir = (transform.forward * z + transform.right * x) * moveSpeed;
+        dir.y = rig.velocity.y;
+    }
+
+    void TryJump ()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 1.5f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        id = player.ActorNumber;
+        photonPlayer = player;
+        GameManager.instance.players[id - 1] = this;
+        
+        if (!photonView.IsMine)
+        {
+            GetComponentInChildren<Camera>().gameObject.SetActive(false);
+            rig.isKinematic = true;
+        }
+    }
+
+}
